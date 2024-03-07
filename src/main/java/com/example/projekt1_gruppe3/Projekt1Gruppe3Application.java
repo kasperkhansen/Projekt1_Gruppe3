4f54991a446b4b4b04864d27d9dd9b1120cbd7be
kasperkhansen <52578478+kasperkhansen@users.noreply.github.com>
2024-03-07 12:30:18 +0100

    Initial Commit

---
+++ src/main/java/com/example/projekt1_gruppe3/Projekt1Gruppe3Application.java
@@ -0,0 +1,13 @@
+package com.example.projekt1_gruppe3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projekt1Gruppe3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Projekt1Gruppe3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projekt1_gruppe3/Projekt1Gruppe3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projekt1_gruppe3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projekt1Gruppe3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
